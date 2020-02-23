586787538df20a685c6e959ac7ca071494727d63
Marcus <davi2812@dee.ufc.br>
2020-02-23 20:25:55 -0300

    grid ainda n ta legal..mas ta funcionando com kdtree

--- Holes/pc_grid_kdtree.m
+++ Holes/pc_grid_kdtree.m
@@ -36,35 +36,56 @@ inflation_factor = 1;
 % max_y = max(Y)*inflation_factor;
 % min_y = min(Y)*inflation_factor;
 
-max_x = max(X) + inflation_factor;
-min_x = min(X) - inflation_factor;
-max_y = max(Y) + inflation_factor;
-min_y = min(Y) - inflation_factor;
-
-max_x_int = ceil(max_x);
-min_x_int = ceil(min_x);
-max_y_int = ceil(max_y);
-min_y_int = ceil(min_y);
+max_x = ceil(max(X)) + inflation_factor;
+min_x = floor(min(X)) - inflation_factor;
+max_y = ceil(max(Y)) + inflation_factor;
+min_y = floor(min(Y)) - inflation_factor;
 
 %largura e altura do grid
-dx = max_x_int - min_x_int;
-dy = max_y_int - min_y_int;
+dx = max_x - min_x;
+dy = max_y - min_y;
+
 % na verdade, nao precisamos fatiar exatamente as dimensões da pilha. pode
 % ser algo mais 'comportado', ou 'quadrado'.
 
 % bolar um algoritmo para que gcd(dx,dy) seja maximizado. o GCD é o tamanho máximo
 % da célula. quato maior, melhor
 
+% encontarando máximo multiplo da menor dimensão mais próximo da maior
+% dimensão
+if (dx > dy)
+    minor_dim = dy;
+    minor_vec = Y;
+    major_dim = dx;
+    major_vec = X;
+else
+    minor_dim = dx; 
+    minor_vec = X;
+    major_dim = dy;
+    major_vec = Y;
+end
 
+minor_multiple = minor_dim;
+factor = 1;
+while (minor_multiple < major_dim)
+    factor = factor+1;
+    minor_multiple = minor_multiple*factor;
+end
+err = minor_multiple - major_dim;
+%recalcula dx
+% testar 
+min_x_corrected = min_x - err/2;
+max_x_corrected = max_x + err/2;
+new_dx = max_x_corrected - min_x_corrected
 
-g_div = gcd(dx,dy)
-
-division_factor = 1; % 0 < div_factor <= 1
+g_div = gcd(new_dx,dy)
+% return
+division_factor = 0.1; % 0 < div_factor <= 1
 cell_length = g_div*division_factor;
 fprintf('Res = %d m\n',cell_length);
 
 % Mesh
-xcells = min_x:cell_length:max_x;
+xcells = min_x_corrected:cell_length:max_x_corrected;
 ycells = min_y:cell_length:max_y;
 
 %Nossa grid foi definida agora.
@@ -105,9 +126,9 @@ plot(qpoints(:,1),qpoints(:,2),'.');
 
 %% KD Tree
 % KD = KDTreeSearcher(xy_rotated,'BucketSize',10,'distance','minkowski','P',1);
-KD = KDTreeSearcher(xy_rotated,'BucketSize',10,'distance','chebychev');
+KD = KDTreeSearcher(xy_rotated,'BucketSize',50,'distance','chebychev');
 
-r = (cell_length/2)*0.95; %raio ~ quadrado de lado 2r!
+r = (cell_length/2); %raio ~ quadrado de lado 2r!
 
 interations = (nx-1)*(ny-1);
 M = zeros(1,interations);
@@ -131,7 +152,7 @@ plot(x_range,y_range,'g.')
 drawnow
 % pause(.1)
 end
-total_ums = sum(M(:));
+total_ums = sum(M(:))
 preenchimento = total_ums / (ncells)
 
 %% KD Tree test
