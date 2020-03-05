3a0da54e71da4178457e40536a0d683647505b09
Marcus <davi2812@dee.ufc.br>
2020-03-05 00:04:10 -0300

    bruteforce e kd ok

--- Registration/my_scanmatch_test_kdtree.m
+++ Registration/my_scanmatch_test_kdtree.m
@@ -11,70 +11,112 @@ Y0 = p0.Location(:,2);
 X1 = p1.Location(:,1);
 Y1 = p1.Location(:,2);
 
-P0 = [nonzeros(X0) nonzeros(Y0)];
-P1 = [nonzeros(X1) nonzeros(Y1)];
 %% Filtros
+
+
+P0 = [nonzeros(X0)'; nonzeros(Y0)'];
+P1 = [nonzeros(X1)'; nonzeros(Y1)'];
+
 n = length(P0);
 n1 = length(P1);
 
 %% a Few plots
 %% Plots
-plot(P0(:,1),P0(:,2),'b.') % Original
+plot(P0(1,:),P0(2,:),'b.') % Original
 hold on
-plot(P1(:,1),P1(:,2),'r.') % Final
+plot(P1(1,:),P1(2,:),'r.') % Final
 drawnow
 %% myICP
-match_iterations = 10;
+match_iterations = 50;
 % Closest point (ñ mt eficiente?)
 
-%% KD TREE for closest
-P0_transformed = P0;
-KD = KDTreeSearcher(P1,'BucketSize',10);
+%% KD TREE for closest. P0 -> Fixed Cloud / MAP ; P1 -> Moving cloud / Last SCAN
+P1_transformed = P1;
+KD = KDTreeSearcher(P0','BucketSize',10);
 r = 1; %distance
+R_ = eye(2);
+T_ = [0 0]';
+tic
 for k=1:match_iterations
-    j = 1;
-    for i=1:n %em P0
-        p0 = P0_transformed(i,:);     
-        IDX_ = rangesearch(KD,p0,r); 
-        IDX = IDX_{1};
-        if(isempty(IDX))
-            
-        else
-        P0_corr(j,:) = p0;
-        P1_corr(j,:) = P1(IDX(1),:);
-        j = j+1;
-        end
-    end
-    plot(P0_corr(:,1),P0_corr(:,2),'o')
-    plot(P1_corr(:,1),P1_corr(:,2),'o')
-    % Match / Registration
-    T = my_scanmatch(P0_corr,P1_corr,[0 0 0]',5);
-    P0_transformed = pc_transform(P0_transformed,T);
-    plot(P0_transformed(:,1),P0_transformed(:,2),'.')
+          k
+        % Find correlations procura KD(P0) por pontos proximos de CADA ponto em P1
+        [match ~] = knnsearch(KD,P1_transformed');
+%           match = brutesearch(P0,P1_transformed);
+        
+        
+        weights = ones(1,length(match));
+        
+
+        p1_idx = true(1, n1);
+        p0_idx = match;
+        
+        P0_corr = P0(:,p0_idx);
+        P1_corr = P1_transformed(:,p1_idx);
+        
+        err_ = (P0_corr - P1_corr)';
+        err = sum(err_);
+        norm(err)
+%         norm(err_)
+    
+%     [R,T] = my_scanmatch(P0_corr,P1_corr,weights,1);
+    [R,T] = eq_point(P0_corr,P1_corr,weights(p1_idx));
+
+    R_ = R*R_; %totals
+    T_ = R*T_ + T; %totals
+    P1_transformed = R * P1_transformed + T;
+    
+    
+    plot(P1_transformed(1,:),P1_transformed(2,:),'g.')
     drawnow
 end
+toc
 
-%% Assumir data assossiation fixa
-P1 = pc_transform(P0,[5 5 0]) %test
-%Plots
-figure
-plot(P0(:,1),P0(:,2),'b.') % Original
-hold on
-plot(P1(:,1),P1(:,2),'r.') % Final
-drawnow
-P0_transformed = P0;
-T = my_scanmatch(P0_transformed,P1,[0 0 0]',10);    
-P0_transformed = pc_transform(P0_transformed,T);
-   plot(P0_transformed(:,1),P0_transformed(:,2),'.')
-   drawnow
+function [R,T] = eq_point(q,p,weights)
+
+m = size(p,2);
+n = size(q,2);
+
+% normalize weights
+weights = weights ./ sum(weights);
+
+% find data centroid and deviations from centroid
+q_bar = q * transpose(weights);
+q_mark = q - repmat(q_bar, 1, n);
+% Apply weights
+q_mark = q_mark .* repmat(weights, 2, 1); %Modificado (weights, 3, 1) -> (weights, 2, 1)
+
+% find data centroid and deviations from centroid
+p_bar = p * transpose(weights);
+p_mark = p - repmat(p_bar, 1, m);
+% Apply weights
+%p_mark = p_mark .* repmat(weights, 3, 1);
+
+N = p_mark*transpose(q_mark); % taking points of q in matched order
+
+[U,~,V] = svd(N); % singular value decomposition
+
+R = V*diag([1 det(U*V')])*transpose(U);
+
+T = q_bar - R*p_bar;
+end
 
 
+function match = brutesearch(p0,p1)
+n0 = length(p0);
+n1 = length(p1);
+match = zeros(n1,1);
+for i=1:n1 %sweep p1
+    mindist = 100000;
+    mindex = 1;
+    for j=1:n0 %sweep p0
+        dist = norm(p0(:,j) - p1(:,i));
+        if dist < mindist
+            mindist = dist;
+            mindex = j;
+        end
+    end
+    match(i) = mindex;
+end
 
 
-
-
-
-
-
-
-
+end
