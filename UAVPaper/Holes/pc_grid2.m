245ba6ed8e43122c4fc08de1bc105393dc64c1aa
Marcus <davi2812@dee.ufc.br>
2020-03-24 16:52:25 -0300

    updatd

--- UAVPaper/Holes/pc_grid2.m
+++ UAVPaper/Holes/pc_grid2.m
@@ -1,6 +1,6 @@
 clear; close all;clc
 % xyz = load('pilha_densa.txt');
-xyz = load('pontos_1574705224_seg.asc');
+xyz = load('pontos_1574705224_seg.asc'); %AQUI VC SELECIONA A NUVEM JÁ SEGMENTADA
 xyz = xyz(:,1:3);
 xy = xyz(:,1:2);
 % pc = pointCloud(xyz)
